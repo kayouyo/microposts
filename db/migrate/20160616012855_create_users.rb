1bb4bed5a31216d5b959b6bae0282635ca6d2926
cyang <cyang@netprotections.co.jp>
2016-06-16 01:34:26 +0000

    add user model@Lesson7

---
+++ db/migrate/20160616012855_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+
+      t.timestamps null: false
+    
+      # unique index 追加: 0616 L6 5.2 マイグレーションの実行
+      t.index :email, unique: true
+    end
+  end
+end
