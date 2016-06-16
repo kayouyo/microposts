b640e66f64899e2837b13518815d77d1b77b914e
cyang <cyang@netprotections.co.jp>
2016-06-16 05:31:36 +0000

    add login function@lesson7

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  # 0616 13597.2 コントローラとルートの作成 ??delete?? bug?
+  # get 'sessions/new'
+  # get 'users/new'
 
   # get 'static_pages/home'
   
@@ -7,7 +9,11 @@ Rails.application.routes.draw do
   # 0616 Lesson7 6.2 ルートの作成
   # set "app/views/static_pages/home.html.erb" as the top page.
   root to: 'static_pages#home'
-  get 'signup',  to: 'users#new'
+  get    'signup', to: 'users#new'
+  get    'login' , to: 'sessions#new'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
